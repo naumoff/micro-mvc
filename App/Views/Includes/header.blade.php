6ad2a7ddd46660369cdad7b054ccae9b5d77bb7e
Andrey Naumoff <andrey.naumoff@gmail.com>
2017-08-07 15:27:15 +0300

    wrong stage completed

--- App/Views/Includes/header.blade.php
+++ App/Views/Includes/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
-    <title>test</title>
+    <title>{{$_REQUEST['c']}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet"
           href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
